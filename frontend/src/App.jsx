0a4ece303d3ba152c77c015aedf68a7067404abd
pasindu <pasindukossinna@gmail.com>
2026-07-01 00:48:19 +0845

    Resolved merge conflict in App.jsx imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AdminLayout from './layouts/AdminLayout';
+import PageHeader from './components/header';
 import Lecturers from './pages/admin/Lecturers';
 import AddStudent from './pages/admin/AddStudent';
-=======
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import AdminLayout from './layouts/AdminLayout';
-import Lecturers from './pages/admin/Lecturers';
 import CourseRegistration from './students/EnrolementPage';
-import PageHeader from './components/header'
->>>>>>> 22006e7b1cff1fbe933a15d58f884bb76e580e3b
 
 function App() {
   const [message, setMessage] = useState("Loading backend status...");
@@ -25,27 +17,22 @@ function App() {
   }, []);
 
   return (
-<BrowserRouter>
+    <BrowserRouter>
       <Routes>
-        {/* Admin Interface */}
+      
         <Route path="/admin" element={
             <AdminLayout pageTitle="Dashboard">
               <h1>Dashboard page</h1>
             </AdminLayout>
         } />
         
-        
-        {/*Lecturer Route */}
+       
         <Route path="/admin/lecturers" element={<Lecturers />} />
-
-<<<<<<< HEAD
         <Route path="/admin/students" element={<AddStudent />} />
-=======
-        {/*Studen Courss Enrolment route */}
+        
         <Route path="/student/registration" element={<CourseRegistration />} />
 
         <Route path="/head" element={<PageHeader />} />
->>>>>>> 22006e7b1cff1fbe933a15d58f884bb76e580e3b
 
       </Routes>
     </BrowserRouter>
